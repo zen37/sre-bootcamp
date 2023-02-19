c097180d366cfb74ad039944c3d69dc6ba5d6ef0
zen <mobi2009ro@gmail.com>
2023-02-19 15:58:38 -0800

    test protected

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -22,7 +23,7 @@ func TestHandleLogin(t *testing.T) {
 		expectedStatus int
 		expectedToken  string
 	}{
-		{"Valid credentials", "bob", "thisIsNotAPasswordBob", http.StatusOK, ""},
+		{"Valid credentials", "bob", "thisIsNotAPasswordBob", http.StatusOK, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidmlld2VyIn0.l7pxJXYHlJdtI9RME2UesMzuVjqf-RtzQeLTHomo_Ic"},
 		{"Invalid username", "invaliduser", "testpassword", http.StatusUnauthorized, ""},
 		{"Invalid password", "testuser", "invalidpassword", http.StatusUnauthorized, ""},
 	}
@@ -64,10 +65,75 @@ func TestHandleLogin(t *testing.T) {
 					t.Errorf("failed to unmarshal response body: %v", err)
 				}
 				actualToken := response["token"]
-				if len(actualToken) == 0 {
-					t.Errorf("HandleLogin did not return a valid token")
+				if actualToken != test.expectedToken {
+					t.Errorf("HandleLogin did not return the expected token for test case %q: got %q, want %q", test.name, actualToken, test.expectedToken)
 				}
 			}
+		})
+	}
+}
+
+func TestHandleProtected(t *testing.T) {
+	// Test cases
+	tests := []struct {
+		name           string
+		role           string
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			name:           "admin user",
+			role:           "admin",
+			expectedStatus: http.StatusOK,
+			expectedBody:   "Protected resource accessed successfully",
+		},
+		{
+			name:           "non-admin user",
+			role:           "user",
+			expectedStatus: http.StatusUnauthorized,
+			expectedBody:   "Unauthorized",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Start a test server with handleProtected as the handler
+			testServer := httptest.NewServer(http.HandlerFunc(handleProtected))
+
+			// Set up a request with a valid JWT token in the Authorization header
+			req, err := http.NewRequest("GET", testServer.URL, nil)
+			if err != nil {
+				t.Fatalf("Failed to create request: %v", err)
+			}
+			token, err := generateToken(tt.role)
+			if err != nil {
+				t.Fatalf("Failed to generate token: %v", err)
+			}
+			req.Header.Set("Authorization", "Bearer "+token)
+
+			// Send the request and get the response
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Fatalf("Failed to send request: %v", err)
+			}
+			defer resp.Body.Close()
+
+			// Check that the response status code is as expected
+			if resp.StatusCode != tt.expectedStatus {
+				t.Errorf("Unexpected status code: got %d, want %d", resp.StatusCode, tt.expectedStatus)
+			}
+
+			// Check that the response body contains the expected message
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("Failed to read response body: %v", err)
+			}
+			actualBody := strings.TrimSpace(string(bodyBytes))
+			if actualBody != tt.expectedBody {
+				t.Errorf("Unexpected response body: got %q, want %q", actualBody, tt.expectedBody)
+			}
+
+			testServer.Close()
 		})
 	}
 }
